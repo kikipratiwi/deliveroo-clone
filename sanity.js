6f704a17b67a9b13111ec5191803d8d4f1907f4e
Kiki Pratiwi <kikipratiwi@gmail.com>
2023-05-10 11:34:32 +0700

    add sanity-client, react-native-url-polyfill

---
+++ sanity.js
@@ -0,0 +1,17 @@
+import createClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+const sanityClient = createClient({
+    projectId: '1o9elwyi',
+    dataset: 'production',
+    useCdn: 'true',
+    apiVersion: '2023-05-08',
+});
+
+const builder = imageUrlBuilder(sanityClient);
+export const urlFor = (source) => builder.image(source);
+
+// RUN THIS to add exception for localhost 3000 CORS policy
+// sanity CORS add http://localhost:3000
+
+export default sanityClient;
