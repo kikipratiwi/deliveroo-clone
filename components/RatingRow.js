2676e0b19843233884a96a8b196f10ea4c07d491
Kiki Pratiwi <itsme.kikipratiwi@gmail.com>
2023-06-22 23:51:17 +0700

    feat(rating): change color

--- components/RatingRow.js
+++ components/RatingRow.js
@@ -5,9 +5,9 @@ import React from 'react';
 export default function RatingRow({ rating, genre }) {
     return (
         <View className="flex-row items-center space-x-1">
-            <StarIcon color="green" opacity={0.5} size={22} />
+            <StarIcon color="#01A296" opacity={0.5} size={22} />
             <Text className="text-xs text-gray-500">
-                <Text className="text-green-500">
+                <Text className="text-[#01A296]">
                     {(Math.floor(rating * 100) / 100).toFixed(1)}
                 </Text>{' '}
                 · {genre}
